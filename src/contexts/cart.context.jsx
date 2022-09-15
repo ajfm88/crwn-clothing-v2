758ead2dfe274e4932d78ad6fadedd1243b078c2
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-14 20:22:11 -0700

    117. Add To Cart Pt.1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,26 @@
 import { createContext, useState } from 'react';
 
+export const addCartItem = (cartItems, productToAdd) => {
+  // find if cartitems contains product toadd
+  // if found, increment quantity
+  // return new array with modified cartitems new cart item
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
