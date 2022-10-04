2a1149fdb26919acef339f81ba301a64ae71cb63
unknown <ajfoucaultmo@mail.usf.edu>
2022-10-03 20:08:52 -0700

    139. Styled-Component - Directory Item & Cart Icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,22 +1,18 @@
-import { useContext } from 'react';
+import { CartItemContainer, ItemDetails } from './cart-item.styles';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
-import { CartContext } from '../../contexts/cart.context';
-
-import './cart-icon.styles.scss';
-
-const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{cartCount}</span>
-    </div>
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
-export default CartIcon;
+export default CartItem;
