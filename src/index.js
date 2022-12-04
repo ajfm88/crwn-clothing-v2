7857ed4445f46d3e024927a6e95c0de7e3aa5b1e
unknown <ajfoucaultmo@mail.usf.edu>
2022-12-04 13:09:26 -0800

    265. Adding mobile and global styles.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { Elements } from "@stripe/react-stripe-js";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
-import App from "./App";
-import { store, persistor } from "./store/store";
-import { stripePromise } from "./utils/stripe/stripe.utils";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import App from './App';
+import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+import * as registerServiceWorker from './serviceWorkerRegistration';
 
-import "./index.scss";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
@@ -29,4 +27,4 @@ render(
   rootElement
 );
 
-serviceWorkerRegistration.register();
+registerServiceWorker.register();
